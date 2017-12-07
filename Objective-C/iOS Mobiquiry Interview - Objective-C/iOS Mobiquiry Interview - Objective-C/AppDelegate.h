10e0bb4ba064e900da413da937c8cfa305ccbebe
brett ohland <bohland@mobiquityinc.com>
2017-12-07 14:29:46 -0500

    Initial Commit

---
+++ Objective-C/iOS Mobiquiry Interview - Objective-C/iOS Mobiquiry Interview - Objective-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS Mobiquiry Interview - Objective-C
+//
+//  Created by Brett Ohland on 12/7/17.
+//  Copyright © 2017 Mobiquity Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Objective-C/iOS Mobiquiry Interview - Objective-C/iOS Mobiquiry Interview - Objective-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS Mobiquiry Interview - Objective-C
+//
+//  Created by Brett Ohland on 12/7/17.
+//  Copyright © 2017 Mobiquity Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
